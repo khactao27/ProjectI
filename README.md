6f95cd7c7fdca97daf146f04c5a71e9966ec643f
Tao D.Khac <tao.duongkhac@gmail.com>
2020-11-05 09:53:27 +0700

    commit first

---
+++ README.md
@@ -0,0 +1,1 @@
+//This project about introduction phone !
---
+++ db.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'phone_db'
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  else console.log("Success!");
+});
+module.exports = connection;
